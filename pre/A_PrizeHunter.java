8ab1ac7739657c7e30d90c5f76602e48421920ee
HwangJenn <76980015+HwangJenn@users.noreply.github.com>
2021-01-10 12:45:05 +0900

    Update A_PrizeHunter.java

--- pre/A_PrizeHunter.java
+++ pre/A_PrizeHunter.java
@@ -3,17 +3,17 @@
 //제2회 코드 페스티벌 본선에 진출하여 b등(1 ≤ b ≤ 64)등을 할 것이다. 단, 진출하지 못했다면 b = 0으로 둔다.
 //제이지는 이러한 가정에 따라, 자신이 받을 수 있는 총 상금이 얼마인지를 알고 싶어한다.
 import java.util.*;
-public class A_PrizeHunter {
+public class A_PrizeHunter { 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int T = scan.nextInt();
+        int T = scan.nextInt(); //테스트 수 입력
         for(int i=0; i<T; i++){
-            int first = scan.nextInt();
-            int second = scan.nextInt();
-            System.out.print(a(first)+b(second));
+            int first = scan.nextInt(); //1회 입력받기(a)
+            int second = scan.nextInt();//2회 입력받기(b)
+            System.out.print(a(first)+b(second));//출력은 상금의 합
         }
-    } public static int a(int first) {
-        if(first == 1)
+    } public static int a(int first) {//a 입력받기
+        if(first == 1) //1회 우승상금 계산하기
             return 5000000;
         else if(2<=first && first<=3)
             return 3000000;
@@ -26,8 +26,8 @@ public class A_PrizeHunter {
         else if(16<=first && first<=21)
             return 100000;
         else return 0;
-    } public static int b(int second) {
-        if(second == 1)
+    } public static int b(int second) {//b 입력받기
+        if(second == 1)//2회 우승상금 계산하기
             return 5120000;
         else if(2<=second && second<=3)
             return 2560000;
